755ad2e9bed125a6ee321a39495876f63c4c90ac
Patrick Brockmann <Patrick.Brockmann@lsce.ipsl.fr>
2017-11-14 12:37:19 +0100

    Changes to use matplotlib.dates.date2num to convert datetime object to Matplotlib dates rather than floor, ceil numpy functions

--- Read_01.py
+++ Read_01.py
@@ -16,6 +16,7 @@ outputFile = sys.argv[2]
 #======================================================
 # Read files as DataFrame
 dates = [datetime.date.today() + datetime.timedelta(days=i) for i in range(-6, 1, 1)]
+# print dates[0], dates[-1]
 
 df = pd.DataFrame()
 for date in dates:
@@ -24,11 +25,13 @@ for date in dates:
     try:
     	df = df.append(pd.read_csv(fileIn, sep='\t'))
     except:
+	print "----> " + fileIn + " not readable"
 	continue
 
 df = df.rename(columns=lambda x: x.strip())            # remove leading space
 
 df['date'] = pd.to_datetime(df['date     time'], format='%Y-%m-%d %H:%M:%S')    # first column is 'date time'
+
 
 df = df.set_index('date')
 df = df.sort_index()
@@ -63,7 +66,7 @@ fig, ax = plt.subplots(figsize=(10,6))
 
 #for i,var in enumerate(['PM-total', 'PM10', 'PM4', 'PM2.5', 'PM1']):
 for i,var in enumerate(['PM10', 'PM2.5']):
-    plt.plot(df_resampled.index, df_resampled[var], linewidth=1, label=var, color=colors[i])
+    plt.plot(df_resampled.index.to_pydatetime(), df_resampled[var], linewidth=1, label=var, color=colors[i])
     
 #ax.xaxis.set_major_locator(HourLocator())
 ax.xaxis.set_major_formatter(DateFormatter('%Y-%m-%d     '))
@@ -74,10 +77,12 @@ ax.xaxis.set_minor_locator(HourLocator(interval=6))
 ax.xaxis.set_minor_formatter(DateFormatter('%H'))
 
 x = ax.get_xlim()
-ax.set_xlim([np.floor(x[0]), np.ceil(x[1])])
+ax.set_xlim([matplotlib.dates.date2num(dates[0]), matplotlib.dates.date2num(dates[-1])])
 y = ax.get_ylim()
 #ax.set_ylim([0, y[1]])
 ax.set_ylim([0, 100])
+
+ax.tick_params(axis='x', direction='in', top='on', which='both')
 
 xplace = np.ceil(x[1])+0.1
 ax.axhline(y=80, color='red', linewidth=2, linestyle='dashed')
@@ -86,19 +91,18 @@ ax.text(xplace, 80, "Seuil d'alerte PM10", ha='left', fontsize=8, style='italic'
 ax.axhline(y=50, color='orange', linewidth=2, linestyle='dashed')
 ax.text(xplace, 50, "Seuil d'information PM10", ha='left', fontsize=8, style='italic', bbox=dict(facecolor='orange'))
 
-plt.legend(loc=2, bbox_to_anchor=(1.0, 1.0))
+plt.legend(loc=2, bbox_to_anchor=(1.0, 1.0), edgecolor='black', borderpad=0.8)
 plt.title("LSCE FIDAS")
 #plt.xlabel('Time')
 plt.ylabel(u'Concentration μg/m3')
-plt.grid(color='gray', axis='both', which='both')
+plt.grid(color='gray', axis='both', which='both', linestyle='dotted')
 
 labels = ax.get_xmajorticklabels()
 plt.setp(labels, rotation=70, fontsize=10)
 labels = ax.get_xminorticklabels()
 plt.setp(labels, fontsize=8)
 
-startDate = int(np.floor(x[0])%2)
-for i in np.arange(startDate, len(dates), 2):
+for i in np.arange(0, len(dates), 2):
     #print dates[i]
     plt.axvspan(dates[i], dates[i]+datetime.timedelta(days=1), color='gray', alpha=0.1)
 
